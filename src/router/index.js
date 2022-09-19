357883eccfddc05394fe4678df490b9e3d3c368a
HR <hos.raz1993@gmail.com>
2022-09-20 00:01:17 +0300

    first-commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,37 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import Vue from 'vue'
+import Router from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import NProgress from 'nprogress'
 
-Vue.use(VueRouter);
+Vue.use(Router)
 
-const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-];
+const router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    },
+  ],
+})
 
-const router = new VueRouter({
-  routes,
-});
+router.beforeEach((routeTo, routeFrom, next) => {
+  NProgress.start()
+  next()
+})
 
-export default router;
+router.afterEach(() => {
+  NProgress.done()
+})
+
+export default router
